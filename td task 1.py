6186cf12780d6d1a11c4be06355a9f24bae4a102
lokeshcodesx-cyber <lokesh.codesx@gmail.com>
2026-01-08 17:55:30 +0530

    Add files via upload

---
+++ td task 1.py
@@ -0,0 +1,14 @@
+a = int(input("enter the first number :"))
+b = int(input("enter the second number :"))
+
+
+#addition
+addition = a+b
+print("addition: ",addition)
+sub = a-b
+print("subtraction: ",sub)
+multi = a*b
+print("multiplication: ",multi)
+division = a/b
+print("division: ",division)
+
---
+++ td task 2.py
@@ -0,0 +1,4 @@
+initial = input("enter your first name: ")
+final = input("enter your last name: ")
+
+print(f"hello,{initial}{final}! Welcome to the python program")
